319332437ee33cfadedb78eb5304e232f92c224d
Danilo Ewerton <danilo.ewe@gmail.com>
2022-11-22 21:19:18 -0300

    H2 database

---
+++ backend/src/main/java/com/daniloewerton/dscatalog/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.daniloewerton.dscatalog.services;
+
+import com.daniloewerton.dscatalog.entities.Category;
+import com.daniloewerton.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+}
