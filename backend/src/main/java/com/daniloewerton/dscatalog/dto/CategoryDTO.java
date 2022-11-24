79977141d16ecbf4f6354f378d0064dacb752194
Danilo Ewerton <danilo.ewe@gmail.com>
2022-11-24 19:35:49 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/daniloewerton/dscatalog/dto/CategoryDTO.java
+++ backend/src/main/java/com/daniloewerton/dscatalog/dto/CategoryDTO.java
@@ -5,6 +5,7 @@ import com.daniloewerton.dscatalog.entities.Category;
 import java.io.Serializable;
 
 public class CategoryDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private Long id;
     private String name;
