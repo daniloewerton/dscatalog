60aaaf5496f807d033967e46d672af39bbf67d7c
Danilo Ewerton <danilo.ewe@gmail.com>
2023-07-04 14:17:44 -0300

    Ajuste na versão #2 - Funcionando

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,25 @@
-import Hello from 'components/Hello';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
   return (
-    <Hello />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
